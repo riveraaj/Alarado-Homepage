860f77de1d61f95ca3938160066f525bbbb14e03
riveraaj <jonathandavidr7@gmail.com>
2023-10-27 20:44:35 -0600

    dark-mode refactory

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,92 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
-    openMobileMenu();
-    toggleDarkMode();
-    openNavDesktop();
+    toggleMenu();
+    toggleTheme();
 })
 
-window.addEventListener('resize', () => {
-    openNavDesktop();
-})
-
-let closeMenuTimeout;
-
-function openMobileMenu() {
-    const $btnMenu = document.querySelector('.nav__logo-menu__btn'),
-        $menu = document.querySelector('.nav__link');
+function toggleMenu() {
+    const $btnMenu = document.querySelector('.nav__logo-menu__btn');
 
     document.addEventListener('click', e => {
-        if (e.target === $btnMenu) {
-            ($btnMenu.classList.contains('isOpen')) ? closeMenu($btnMenu, $menu) : openMenu($btnMenu, $menu);
+        if (e.target === $btnMenu){
+            let $menu = document.querySelector('.nav__link');
+            $menu.classList.toggle('isOpen');
+            ($menu.classList.contains('isOpen')) ? $btnMenu.innerHTML = 'close' : $btnMenu.innerHTML = 'menu'
         }
     });
 }
 
-function toggleDarkMode() {
-    const $btnSwitch = document.querySelector('.nav__link__btn-dark-mode'),
-        $body = document.body,
-        $parentHeroText = document.querySelector('.hero__text'),
-        $tittleDarkMode = $parentHeroText.querySelector('h1'),
-        $spanDarkMode = $parentHeroText.querySelector('span'),
-        $pathLogo = document.querySelectorAll('.nav__logo-menu_link__path'),
-        $firstLink = document.querySelector('.nav__link__list__item');
-
-    $btnSwitch.addEventListener('click', () => {
-        ($btnSwitch.classList.contains('inDarkMode')) ? switchToLightMode($body, $tittleDarkMode, $spanDarkMode, $btnSwitch, $pathLogo, $firstLink) : switchToDarkMode($body, $tittleDarkMode, $spanDarkMode, $btnSwitch, $pathLogo, $firstLink);
-
+function toggleTheme() {
+    const $btnSwitch = document.getElementById('switch-mode');
+        
+    document.addEventListener('click', e => {
+        if (e.target === $btnSwitch){
+            document.body.toggleAttribute('data-dark-mode');
+        }
     });
-}
-
-function openNavDesktop() {
-    const $menu = document.querySelector('.nav__link');
-
-    if (window.innerWidth > 1023) {
-        $menu.classList.remove('hidden');
-    } else {
-        $menu.classList.add('hidden');
-    }
-}
-
-const openMenu = (btnMenu, menuContainer) => {
-    menuContainer.classList.remove('hidden');
-    btnMenu.classList.add('isOpen');
-    btnMenu.textContent = 'close';
-    menuContainer.style.display = 'flex';
-}
-
-const closeMenu = (btnMenu, menuContainer) => {
-    if (closeMenuTimeout) clearTimeout(closeMenuTimeout);
-
-    menuContainer.classList.add('hidden');
-    btnMenu.classList.remove('isOpen');
-    btnMenu.textContent = 'menu';
-
-    setTimeout(() => {
-        menuContainer.style.display = 'none';
-        closeMenuTimeout = null;
-    }, 500)
-}
-
-function cancelCloseMenu() {
-    if (closeMenuTimeout) {
-        clearTimeout(closeMenuTimeout);
-        closeMenuTimeout = null;
-    }
-}
-
-const switchToLightMode = (body, tittleDarkMode, spanDarkMode, btnSwitch, pathLogo, firstLink) => {
-    body.classList.remove('dark-mode');
-    tittleDarkMode.classList.remove('dark-mode');
-    spanDarkMode.classList.remove('dark-mode');
-    btnSwitch.classList.remove('inDarkMode');
-    pathLogo.forEach(x => x.classList.remove('dark-mode'));
-    firstLink.classList.remove('dark-mode');
-}
-
-const switchToDarkMode = (body, tittleDarkMode, spanDarkMode, btnSwitch, pathLogo, firstLink) => {
-    body.classList.add('dark-mode');
-    tittleDarkMode.classList.add('dark-mode');
-    spanDarkMode.classList.add('dark-mode');
-    btnSwitch.classList.add('inDarkMode');
-    pathLogo.forEach(x => x.classList.add('dark-mode'));
-    firstLink.classList.add('dark-mode');
 }
