3632ad6c7f65381d2f5462d8d5d9eb125771f1f3
Jonathan Rivera <96964349+riveraaj@users.noreply.github.com>
2024-05-02 13:34:43 -0600

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,26 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    toggleMenu();
-    toggleTheme();
-})
-
 function toggleMenu() {
     const $btnMenu = document.querySelector('.nav__logo-menu__btn');
-
-    document.addEventListener('click', e => {
-        if (e.target === $btnMenu){
-            let $menu = document.querySelector('.nav__link');
-            $menu.classList.toggle('isOpen');
-            ($menu.classList.contains('isOpen')) ? $btnMenu.innerHTML = 'close' : $btnMenu.innerHTML = 'menu'
-        }
-    });
+    let $menu = document.querySelector('.nav__link');
+    $menu.classList.toggle('isOpen');
+    ($menu.classList.contains('isOpen')) ? $btnMenu.textContent = 'close' : $btnMenu.textContent = 'menu'
 }
 
 function toggleTheme() {
-    const $btnSwitch = document.getElementById('switch-mode');
-        
-    document.addEventListener('click', e => {
-        if (e.target === $btnSwitch){
-            document.body.toggleAttribute('data-dark-mode');
-        }
-    });
+    const $btnSwitch = document.getElementById('switch-mode');   
+    document.body.toggleAttribute('data-dark-mode');
 }
+
+window.addEventListener('click', e => {
+    if(e.target.matches('switch-mode')) toggleTheme();
+    if(e.target.matches('.nav__logo-menu__btn')) toggleMenu();  
+});
