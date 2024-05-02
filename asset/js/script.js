c3f458596a053469588537a74c50b127d2ec25a6
Jonathan Rivera <96964349+riveraaj@users.noreply.github.com>
2024-05-02 13:41:36 -0600

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,18 @@
+const $btnMenu = document.querySelector('.nav__logo-menu__btn');
+const $btnSwitch = document.getElementById('switch-mode');   
+const $menu = document.querySelector('.nav__link');
+const $body = document.body;
+
 function toggleMenu() {
-    const $btnMenu = document.querySelector('.nav__logo-menu__btn');
-    let $menu = document.querySelector('.nav__link');
     $menu.classList.toggle('isOpen');
-    ($menu.classList.contains('isOpen')) ? $btnMenu.textContent = 'close' : $btnMenu.textContent = 'menu'
+    $btnMenu.textContent = ($menu.classList.contains('isOpen')) ? 'close' : 'menu';
 }
 
 function toggleTheme() {
-    const $btnSwitch = document.getElementById('switch-mode');   
     document.body.toggleAttribute('data-dark-mode');
 }
 
 window.addEventListener('click', e => {
-    if(e.target.matches('switch-mode')) toggleTheme();
-    if(e.target.matches('.nav__logo-menu__btn')) toggleMenu();  
+    if (e.target === $btnSwitch) toggleTheme();
+    else if (e.target === $btnMenu) toggleMenu();  
 });
